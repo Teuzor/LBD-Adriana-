b6fdd95e43039f8f2fbb13db12cbffc72d546574
Teuzor <mateusdanielsantos926@gmail.com>
2026-08-13 16:18:19 -0300

    Exercicio 2 - Verificar Divisivel

---
+++ Divisivel.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Divisivel {
+    
+    public static void main(String[] args) {
+
+    float numero1, numero2;
+
+    Scanner s=new Scanner (System.in);
+    System.out.println("Insira o primeiro número: ");
+    numero1=s.nextFloat();
+
+    System.out.println("Insira o segundo número: ");
+    numero2=s.nextFloat();
+    
+    if (numero1%numero2==0){
+        System.out.println("O número " + numero1 + " é divisível por " + numero2);
+    } else {
+        System.out.println("O número " + numero1 + " não é divisível por " + numero2);
+
+    }
+}
+}
+
+
