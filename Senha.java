377f0e8aab0fd6785a667357a95b32a929bca4f6
Teuzor <mateusdanielsantos926@gmail.com>
2026-08-13 16:17:08 -0300

    Exercicio 1 - Senha

---
+++ Senha.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Senha {
+    public static void main(String[] args) {
+
+    int senha;
+    int senhacorreta = 1234;
+      
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Insira sua senha: ");
+    senha = scanner.nextInt();
+
+    if (senha == senhacorreta) {
+        System.out.println("Acesso concedido!");
+
+    }  
+    else {
+        System.out.println("Acesso negado!"); 
+
+    
+
+
+    }
+    }
+    }
+
